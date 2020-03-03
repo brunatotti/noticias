8257be063054c499d59d6e54fcdec27c8ece1468
bruna totti <totti_bruna@hotmail.com>
2020-03-03 08:06:31 -0300

    aula 03-03: alterações texto app

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 Este trabalho foi desenvolvido para uma das disciplinas ministradas no Curso **Análise e Desenvolvimento de Sistemas** da Faculdade de Tecnologia de São José dos Campos - Prof Jessen Vidal.
 
 * **Disciplina:** Laboratório de Engenharia de Software
-* **Professor:** Fabrício 
+* **Professor:** Fabrício
 * **Carga Horário:** 80 horas
 * **Assuntos trabalhados:** Java; Gradle; Servlet; JPA; Hibernate; MVC;  JSP; Apache TomCat; JavaScript; JS Closures; JS DOM Handlers; JS event listeners; JQuery; HTML 5; CSS 3; Media queries; PostgreSQL.
 
--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,13 +4,13 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.WebServlet;
 import java.io.PrintWriter;
 
-@WebServlet(urlPatterns={"/app"})
+@WebServlet(urlPatterns={"/"})
 public class App extends HttpServlet{	
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse res){
 		try{
 			PrintWriter pw = res.getWriter();
-			pw.write("Hello everybody!");
+			pw.write("<h1>Hello, professor!!</h1> <i>ass: Bruna Totti</i>");
 			pw.close();
 		} catch (Exception e){
 			System.out.println("Erro em IO ou no Servlet");
